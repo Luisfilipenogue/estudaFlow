21d78889b1763e2b2af7805da8fd7e9cc9b78eea
Luisfilipenogue <lulipearaujo@gmail.com>
2026-06-14 14:37:38 -0300

    fix: formatar src/app.py

--- src/app.py
+++ src/app.py
@@ -151,17 +151,18 @@ class EstudaFlowApp:
 
         def lbl(t):
             return tk.Label(form, text=t, bg="#1A1D2E", fg="#94A3B8", font=("Courier New", 9))
+
         def ent(w=22):
             return tk.Entry(
-                    form,
-                    width=w,
-                    bg="#252840",
-                    fg="#E2E8F0",
-                    insertbackground="#E2E8F0",
-                    font=("Courier New", 10),
-                    relief="flat",
-                    bd=4,
-                )
+                form,
+                width=w,
+                bg="#252840",
+                fg="#E2E8F0",
+                insertbackground="#E2E8F0",
+                font=("Courier New", 10),
+                relief="flat",
+                bd=4,
+            )
 
         lbl("Título *").grid(row=1, column=0, sticky="w")
         self.task_title = ent(26)
@@ -241,17 +242,18 @@ class EstudaFlowApp:
 
         def lbl(t):
             return tk.Label(form, text=t, bg="#1A1D2E", fg="#94A3B8", font=("Courier New", 9))
+
         def ent(w=22):
             return tk.Entry(
-                    form,
-                    width=w,
-                    bg="#252840",
-                    fg="#E2E8F0",
-                    insertbackground="#E2E8F0",
-                    font=("Courier New", 10),
-                    relief="flat",
-                    bd=4,
-                )
+                form,
+                width=w,
+                bg="#252840",
+                fg="#E2E8F0",
+                insertbackground="#E2E8F0",
+                font=("Courier New", 10),
+                relief="flat",
+                bd=4,
+            )
 
         lbl("Nome *").grid(row=1, column=0, sticky="w")
         self.subj_name = ent()
